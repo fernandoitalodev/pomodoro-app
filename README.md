ec0ecd718c8ce86ddb1244bf1e903a445c3021a2
fernando italo <fernando.italo.039@gmail.com>
2023-05-03 15:29:59 -0300

    adicionando o babel

---
+++ README.md
@@ -0,0 +1,2 @@
+#instalando o babel:
+ npm install --save-dev @babel/core @babel/preset-env @babel/preset-react
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+
+import babel from '@rollup/plugin-babel';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' })]
 })
