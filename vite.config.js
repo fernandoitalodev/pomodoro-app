fd6b52029be89cfc0932fdb0c774872fe0b28ac5
fernando italo <fernando.italo.039@gmail.com>
2023-05-03 16:05:15 -0300

    babel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 
 import babel from '@rollup/plugin-babel';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' })]
+  plugins: [react(), babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),reactRefresh()]
 })
